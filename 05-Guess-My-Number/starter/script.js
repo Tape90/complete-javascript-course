57e15784642c4ab1ae167796e5481741e3bf73d5
Tape90 <119531778+Tape90@users.noreply.github.com>
2023-06-03 13:27:53 +0200

    exercise

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,47 @@
 'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20 + 1);
+let score = 20;
+let highScore = 0;
+
+function displayMessage (incMessage) {
+    document.querySelector('.message').textContent = incMessage;
+}
+
+document.querySelector('.check').addEventListener('click', () => {
+    const guess = Number(document.querySelector('.guess').value);
+
+document.querySelector('.again').addEventListener('click', () => {
+    score = 20;
+    document.querySelector('.score').textContent = score;
+    secretNumber = Math.trunc(Math.random() * 20 + 1);
+    displayMessage('Guess My Number!');
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
+})
+
+    if(!guess) {
+        displayMessage('⚠️ No number!');
+
+    } else if(guess === secretNumber) {
+        displayMessage('🎉 Number is correct');
+        score > highScore ? highScore = score : null;
+        document.querySelector('.highscore').textContent = highScore;
+        document.querySelector('.number').textContent = secretNumber;
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = '30rem';
+
+    } else if (guess !== secretNumber) {
+        if (score > 1) {
+            displayMessage(guess > secretNumber ? '↗️ To high' : '↘️ To low');
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            displayMessage('💀 You lost the game');
+            document.querySelector('body').style.backgroundColor = '#eb0909';
+            document.querySelector('.score').textContent = 0;
+        }
+    }
+})
