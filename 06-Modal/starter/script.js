d92ec029059f1381c3d72d610972d7b52ab72778
Tape90 <119531778+Tape90@users.noreply.github.com>
2023-06-04 13:54:40 +0200

    modal

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -5,5 +5,23 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
+function closeModal () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+
+function openModal () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
 for(let i = 0; i < btnsOpenModal.length; i++) 
-    console.log(btnsOpenModal[i].textContent);
+    btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape' && !modal.classList.contains('hidden')) closeModal();
+});
