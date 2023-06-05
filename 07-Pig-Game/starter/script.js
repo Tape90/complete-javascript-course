697069e5c3275a3c685972121fa297b9610813ac
Hieu Ngo <trunghieu.ngo83@gmail.com>
2023-06-05 16:26:28 +0200

    Pig Game

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,5 +1,77 @@
 'use strict';
 
-let scorePOne = document.querySelector('#score--0').textContent;
-let scorePTwo = document.querySelector('#score--1').textContent;
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+const score0El = document.getElementById('score--0');
 
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+let scores, currentScore, activePlayer, playing;
+
+function startGame() {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  diceEl.classList.add('hidden');
+};
+startGame();
+
+function switchPlayer() {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
+
+btnRoll.addEventListener('click', () => {
+  if (playing) {
+    const dice = Math.trunc(Math.random() * 6 + 1);
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+    if (dice === 1) {
+      switchPlayer();
+    } else {
+      currentScore = currentScore + dice;
+      document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+    };
+  };
+});
+
+btnHold.addEventListener('click', () => {
+  if (playing) {
+    scores[activePlayer] = scores[activePlayer] + currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
+
+    if (scores[activePlayer] >= 100) {
+      document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
+      diceEl.classList.add('hidden');
+      playing = false;
+    } else {
+      switchPlayer();
+    }
+  }
+});
+
+
+btnNew.addEventListener('click', () => {
+  if (activePlayer) {  // 0 = false, Player 2 = 1 und somit true
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+  };
+  startGame();
+});
--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+var birthYear = 1990;
+
+const jens = {
+  //birthYear: 1990,
+  // toString: function () {
+  //   console.log('I overwrite it')
+  // },
+  age: () => {
+    //console.log(this)
+    console.log(2023 - this.birthYear);
+  }
+}
+
+jens.age();
+console.log(jens);
